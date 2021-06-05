7f031556e4d10598ad17519d846d0d4735b2bce9
soulsayer9029 <dhruvjain9029@gmail.com>
2021-06-05 19:09:20 +0530

    attchments taken care of

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const express=require('express')
 const app=express()
 const cors=require('cors')
 const nodemailer=require('nodemailer')
+const bodyParser=require('body-parser')
+
+var multer = require('multer');
+var upload = multer();
+// app.use(upload.array()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 require('dotenv/config')
 var transporter=nodemailer.createTransport({
     service:'Gmail',
     auth:{
-        user:'nodeforme@gmail.com',
+        user:'devolver.tech@gmail.com',
         pass:process.env.PASSWORD
 
     },
@@ -16,22 +23,22 @@ var transporter=nodemailer.createTransport({
 
 
 app.use(cors())
-const bodyParser=require('body-parser')
-app.use(bodyParser.json())
+
 
 
 const port=process.env.PORT || 3000
 
+  
 
 app.get('/',(req,res)=>{
     res.send('<h1>Noice</h1>')
 })
 
 app.post('/sendmail',async(req,res)=>{
-    const message=req.body.message
+    // const message=req.body.message
     
     transporter.sendMail({
-        from:'nodeforme@gmail.com',
+        from:'devolver.tech@gmail.com',
         to:req.body.recieverEmail,
         subject:'Query',
         text:`  Sender E-mail:${req.body.senderEmail} \n Message: ${req.body.message} \n mobile number  ${req.body.mobile} \n scope:${req.body.scope} \n client:${req.body.client}`
@@ -44,7 +51,32 @@ app.post('/sendmail',async(req,res)=>{
         }
     })
 })
-
+app.post('/sendmailAttachment',upload.array('files',2),async(req,res)=>{
+    // const message=req.body.message
+    // console.log(req.body)
+    console.log(req.files);
+    transporter.sendMail({
+        from:'nodeforme@gmail.com',
+        to:req.body.recieverEmail,
+        subject:'Query',
+        text:`  Sender E-mail:${req.body.senderEmail} \n Position: ${req.body.position} \n mobile number : ${req.body.mobile} \n name:${req.body.name} \n`,
+        attachments:[{  
+            filename: "cv.pdf",
+            content:req.files[0].buffer
+        },{  
+            filename: "portfolio.pdf",
+            content:req.files[1].buffer
+        }]
+    },(error,info)=>{
+        if(error){
+            console.log("hii")
+            // console.log(req);
+            res.status(400).send(error)
+        }else{
+           res.status(200).send("mail sent")
+        }
+    })
+})
 app.listen(port,()=>console.log("Server is listening"))
 
 
